5bd42955790604bbd1895e1662e49647234c84a9
odinokun <odinokun@gmail.com>
2024-11-18 16:58:06 -0500

    Remove commented-out code and streamline axios usage for fetching todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,6 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
-// Hi guys! Let`s reinforce our session:
-
-// 1. Install AXIOS -it`s a library for HTTP requests. We  use it instead method FETCH.
-// https://axios-http.com/ru/docs/intro
-// yarn add axios
-
-// axios.get('https://jsonplaceholder.typicode.com/todos')
-//     .then((res) => {
-//         setTodos(res.data)
-//     })
-
-//2. Let`s relocate our method map, and wrap it in a new variable:
-//const mapTodos=todos.map(el => {...
-
-// return (
-//     <div className="App">
-//         <button onClick={onClickHandler}>CLEAN POSTS</button>
-//         <ul>
-//             {mapTodos}
-//         </ul>
-//     </div>
-// );
-
 // 3. Create new button to redisplay  our data
 
 // 4. We are having a problem. The code is duplicated (axios.get...). Let`s create a new function and use it where we need.
@@ -41,9 +18,6 @@ function App() {
   const [todos, setTodos] = useState<PropsType[]>([]);
 
   useEffect(() => {
-    // fetch('https://jsonplaceholder.typicode.com/todos')
-    //   .then(response => response.json())
-    //   .then(json => setTodos(json));
     axios.get('https://jsonplaceholder.typicode.com/todos').then(res => {
       setTodos(res.data);
     });
